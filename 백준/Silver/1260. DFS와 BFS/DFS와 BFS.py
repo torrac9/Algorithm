e93ba531fc1ae0bf003ee42e7fa8e1befadbd958
torrac9 <129646212+torrac9@users.noreply.github.com>
2023-04-21 23:47:25 +0900

    [Silver II] Title: DFS와 BFS, Time: 684 ms, Memory: 34200 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,50 @@
+import sys
+from collections import deque
+
+
+def bfs(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        i = queue.popleft()
+        if i not in visited:
+            visited.append(i)
+            if i in graph:
+                temp = list(set(graph[i])-set(visited))
+                temp.sort()
+                queue += temp
+    return ' '.join(str(i) for i in visited)
+
+def dfs(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        i = stack.pop()
+        if i not in visited:
+            visited.append(i)
+            if i in graph:
+                temp = list(set(graph[i])-set(visited))
+                temp.sort(reverse = True)
+                stack += temp
+    return ' '.join(str(j) for j in visited)
+
+graph = {}
+
+node, edge, start = map(int, sys.stdin.readline().split())
+for i in range(edge):
+    edge_info = input().split(' ')
+    n1, n2 = [int(j) for j in edge_info]
+    if n1 not in graph:
+        graph[n1] = [n2]
+    elif n2 not in graph[n1]:
+        graph[n1].append(n2)
+
+    if n2 not in graph:
+        graph[n2] = [n1]
+    elif n1 not in graph[n2]:
+        graph[n2].append(n1)
+
+print(dfs(graph, start))
+print(bfs(graph, start))
