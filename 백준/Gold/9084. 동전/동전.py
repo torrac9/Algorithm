9ee91a4f60ee8eef4442f79eff307fd487d22289
torrac9 <129646212+torrac9@users.noreply.github.com>
2023-04-28 15:06:39 +0900

    [Gold V] Title: 동전, Time: 144 ms, Memory: 35420 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,20 @@
+import sys
+#sys.stdin = open("C:\\Users\\82103\\Desktop\\Krafton Jungle\\Krafton04\\input.txt", "r")
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):      #2차원 배열
+    n = int(sys.stdin.readline().rstrip())
+    coin = list(map(int, sys.stdin.readline().split()))
+    coin.insert(0,0)
+    m = int(sys.stdin.readline())
+
+    dp = [[0] *(m+1) for _ in range(n+1)]
+    for i in range(n+1):
+        dp[i][0] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            dp[i][j] = dp[i-1][j]
+            if j - coin[i] >= 0:
+                dp[i][j] += dp[i][j-coin[i]]
+    print(dp[n][m])
