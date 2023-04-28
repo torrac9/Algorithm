cd9f8e12c583d3a99cc084cdc41b494772b112e1
torrac9 <129646212+torrac9@users.noreply.github.com>
2023-04-28 15:16:19 +0900

    [Gold V] Title: 동전, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -3,18 +3,26 @@ import sys
 
 t = int(sys.stdin.readline())
 
-for _ in range(t):      #2차원 배열
+for _ in range(t):      
     n = int(sys.stdin.readline().rstrip())
     coin = list(map(int, sys.stdin.readline().split()))
-    coin.insert(0,0)
+    #coin.insert(0,0)
     m = int(sys.stdin.readline())
 
-    dp = [[0] *(m+1) for _ in range(n+1)]
-    for i in range(n+1):
-        dp[i][0] = 1
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            dp[i][j] = dp[i-1][j]
-            if j - coin[i] >= 0:
-                dp[i][j] += dp[i][j-coin[i]]
-    print(dp[n][m])
+    # dp = [[0] *(m+1) for _ in range(n+1)]       #2차원 배열
+    # for i in range(n+1):
+    #     dp[i][0] = 1
+    # for i in range(1, n+1):
+    #     for j in range(1, m+1):
+    #         dp[i][j] = dp[i-1][j]
+    #         if j - coin[i] >= 0:
+    #             dp[i][j] += dp[i][j-coin[i]]
+    # print(dp[n][m])
+
+    dp = [0]*(m+1)      #1차원 배열
+    dp[0] = 1
+    for c in coin:
+        for i in range(1, m+1):
+            if i >= c:
+                dp[i] += dp[i-c]
+    print(dp[m])
