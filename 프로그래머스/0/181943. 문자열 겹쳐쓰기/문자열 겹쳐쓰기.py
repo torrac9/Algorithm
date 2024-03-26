374c99a1d3feb9db72db69e0a47ab6b8af404a7a
torrac9 <129646212+torrac9@users.noreply.github.com>
2024-03-26 23:35:55 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,7 @@
+def solution(my_string, overwrite_string, s):
+    answer = ''
+    answer += my_string[:s]
+    answer += overwrite_string
+    if len(answer) < len(my_string):
+        answer += my_string[s+len(overwrite_string):]
+    return answer
