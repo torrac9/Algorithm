047f885b5193bf80302b10d5c6dc50114fa53ebb
torrac9 <129646212+torrac9@users.noreply.github.com>
2024-04-03 22:00:53 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,11 @@
+import sys
+
+#sys.stdin = open("C:/Users/이동근/Desktop/공부/1주차/input.txt", "r")
+
+A, B = map(int, sys.stdin.readline().split())
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
