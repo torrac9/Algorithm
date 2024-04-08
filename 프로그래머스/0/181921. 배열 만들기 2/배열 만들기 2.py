2689d77fde3941a71f924fa0a7b378d68808cb35
torrac9 <129646212+torrac9@users.noreply.github.com>
2024-04-08 23:46:05 +0900

    [level 0] Title: 배열 만들기 2, Time: 16.82 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,7 @@
+def solution(l, r):
+    answer = []
+    for i in range(l, r+1):
+        if '1' not in str(i) and '2' not in str(i) and '3' not in str(i) and '4' not in str(i) and '6' not in str(i) and '7' not in str(i) and '8' not in str(i) and '9' not in str(i):
+            answer.append(i)
+    
+    return answer if answer else [-1]
